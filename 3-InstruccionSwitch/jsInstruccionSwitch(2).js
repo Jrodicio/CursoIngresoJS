7029c6cae4eb65d1e1f482819d7af70a8b2adfa7
alumno <alumno@utnfra.com.ar>
2020-02-12 20:33:23 -0300

    Se resuelven ejercicios 2 y 3 de SWITCH.

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,41 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
+    
+    /*
+    Comentamos que meses estan en cada estacion.
+    preInvierno = ["Enero","Febrero","Marzo","Abril","Mayo","Junio"];
+    invierno = ["Julio","Agosto"];
+    postInvierno = ["Septiembre","Octubre","Noviembre","Diciembre"];
+    */
+    
+    
+    switch (mesDelAño) {
+        // Case de preInvierno
+        case "Enero":
+        case "Febrero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+            alert("Falta para el invierno.");
+            break;
+        // Case de invierno
+        case "Julio":
+        case "Agosto":
+            alert("Abrigate que hace frio.");
+            break;
+        // Case de postInvierno
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+            alert("Ya pasamos el frío, ahora calor!!!");
+        default:
+            break;
+    }
 
-alert (mesDelAño);
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,8 +3,37 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+/*
+31 dias: Enero;Marzo;Mayo;Julio;Agosto;Octubre;Diciembre
+30 dias: Abril;Junio;Septiembre;Noviembre
+28 dias: Febrero;
+*/
 //alert (mesDelAño);
-	
+	switch (mesDelAño) {
+        // Case para 31 dias
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+            alert("Tiene 31 días");
+            break;
+        // Case para 30 días
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert("Tiene 30 días");
+            break;
+        // Case de 28 días
+        case "Febrero":
+            alert("Tiene 28 días");
+            break;
+        default:
+            break;
+    }
 	
 
 
