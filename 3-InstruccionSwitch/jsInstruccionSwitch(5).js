7358ceb8c19e7ca53df85ee1192742dcc790aa46
alumno <alumno@utnfra.com.ar>
2020-02-12 21:04:16 -0300

    Se resuelven todos los ejercicios SWITCH.

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+    var laHora = document.getElementById('hora').value;
+
+    laHora = parseInt(laHora);
 
 //alert (laHora);
+	switch (true) {
+        case (laHora >= 7 && laHora <= 11):
+            alert("Es de mañana");
+            break;
+    
+        default:
+            break;
+    }
 	
-	
-
-
-
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -1,8 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+    var laHora = document.getElementById('hora').value;
+    laHora = parseInt(laHora);
 
+    switch (true) {
+        
+        case (laHora >= 7 && laHora <= 11):
+            alert("Es de mañana.");
+            break;
+        
+        case (laHora >= 12 && laHora <= 19):
+            alert("Es de tarde.");
+            break;
+        
+        case (laHora >= 20 && laHora <= 24):
+        case (laHora >= 0 && laHora <= 6):
+            alert("Es de noche.");
+            break;
+        default:
+            alert("La hora no existe.");
+            break;
+    }
 
 
 }//FIN DE LA FUNCIÓN
