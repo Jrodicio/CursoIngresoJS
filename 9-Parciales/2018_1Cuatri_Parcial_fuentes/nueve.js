abc290465d340956dcdedfc1d3e246c71218808d
alumno <alumno@utnfra.com.ar>
2020-02-27 20:31:19 -0300

    Se resuelven ej 7 8 y 9 de parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,92 @@
 function mostrar()
 {
+    var marcaProducto;
+    var pesoProducto;
+    var temperaturaAlmacenamientoProducto;
+    var temperaturasPares = 0;
+    var marcaProductoMasPesado;
+    var cantidadProductosTempBajoCero = 0;
+    var acumuladorPeso =0;
+    var contadorProductos = 0;
+    var promedioPeso;
+    var pesoMaximo = 0;
+    var pesoMinimo = 101;
+    var ingresoProducto = false;
+    var continuar = true;
+debugger;
+    while (continuar){
+        //Solicitamos marca, si es null salimos.
+        marcaProducto = prompt("Ingrese la marca del producto");
+        if (marcaProducto == null){
+            break;
+        }
+        
+        //Solicitamos peso. Si es null salimos, si no es un valor valido, volvemos a pedir.
+        do{
+            pesoProducto = prompt("Ingrese el peso del producto (1 a 100)");
+            if (pesoProducto == null){
+                break;
+            }
+            pesoProducto = parseInt(pesoProducto);
+        }while (isNaN(pesoProducto) || pesoProducto > 100 || pesoProducto < 1);
+        if (pesoProducto == null){
+            break;
+        }
 
+        //Solicitamos temperatura. Si es null salimos, si no es un valor valido, volvemos a pedir.
+        do{
+            temperaturaAlmacenamientoProducto = prompt("Ingrese la temperatura de almacenamiento del producto (-30 a 30)");
+            if (temperaturaAlmacenamientoProducto == null){
+                break;
+            }
+            temperaturaAlmacenamientoProducto = parseInt(temperaturaAlmacenamientoProducto);
+        }while (isNaN(temperaturaAlmacenamientoProducto) || temperaturaAlmacenamientoProducto > 30 || temperaturaAlmacenamientoProducto < -30);
+        if (temperaturaAlmacenamientoProducto == null){
+            break;
+        }        
+
+        //Si ingreso todo, comenzamos a contar los productos ingresados
+        
+        //D
+        acumuladorPeso += pesoProducto;
+        contadorProductos++;
+        ingresoProducto = true;
+
+        //A
+        if (temperaturaAlmacenamientoProducto % 2 == 0){
+            temperaturasPares++;
+        }
+
+        //C
+        if (temperaturaAlmacenamientoProducto < 0){
+            cantidadProductosTempBajoCero++;
+        }
+
+        //B y F
+        if (pesoProducto > pesoMaximo){
+            pesoMaximo = pesoProducto;
+            marcaProductoMasPesado = marcaProducto;
+        }
+        if (pesoProducto < pesoMinimo){
+            pesoMinimo = pesoProducto;
+        }
+
+        continuar = confirm("¿Desea continuar?");
+    }
+
+    if (ingresoProducto){
+        promedioPeso = acumuladorPeso/contadorProductos;
+    }
+    else{
+        marcaProductoMasPesado="-";
+        promedioPeso="-";
+        pesoMaximo="-";
+        pesoMinimo="-";
+    }
+
+    document.write("a- Temperaturas pares: "+temperaturasPares+"<br>");
+    document.write("b- Marca de producto más pesado: "+marcaProductoMasPesado+"<br>");
+    document.write("c- Cantidad de productos que se conservan a menos de 0 grados: "+cantidadProductosTempBajoCero+"<br>");
+    document.write("d- Promedio peso: "+promedioPeso+"<br>");
+    document.write("f- Peso máximo ; Peso mínimo: "+pesoMaximo+" ; "+pesoMinimo);
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -9,15 +9,17 @@ function mostrar()
     var contadorImpares = 0;
     var contadorCeros = 0;
     var promedioPositivos;
-    var sumaPositivos;
-    var contadorPositivos;
-    var sumaNegativos;
+    var sumaPositivos = 0;
+    var contadorPositivos = 0;
+    var sumaNegativos = 0;
     var numeroMinimo = 101;
     var letraMinimo;
     var numeroMaximo = -101;
     var letraMaximo;
+    var ingresoPositivo = false;
     //Comenzamos bucle while
 
+debugger;
     while (continuar){
         
         //Ingresamos valor letra y validamos
@@ -58,25 +60,26 @@ function mostrar()
 
         
         //Una vez validados los dos datos, realizamos cálculos
-        contador++;
-        
+                
         //Verificamos pares e impares
         if (numeroIngresado % 2 == 0){
             contadorPares++;
+            //Contamos ceros
+            if (numeroIngresado == 0){
+                contadorCeros++;
+            }
         }
         else{
             contadorImpares++;
         }
 
-        //Contamos ceros
-        if (numeroIngresado == 0){
-            contadorCeros++;
-        }
+        
 
         //Acumulamos positivos y contamos
         if (numeroIngresado > 0){
             sumaPositivos += numeroIngresado;
             contadorPositivos++;
+            ingresoPositivo = true
         }
         //Sino, acumulamos negativos
         else {
@@ -87,7 +90,7 @@ function mostrar()
             numeroMaximo = numeroIngresado;
             letraMaximo = letraIngresada;
         }
-        if (numeroIngresado < numeroMaximo){
+        if (numeroIngresado < numeroMinimo){
             numeroMinimo = numeroIngresado;
             letraMinimo = letraIngresada;
         }
@@ -95,4 +98,29 @@ function mostrar()
         continuar = confirm("¿Desea continuar?");
     }
 
+
+    //Devoluciones
+    /**
+      *  a) La cantidad de números pares. 
+      *  b) La cantidad de números impares. 
+      *  c) La cantidad de ceros. 
+      *  d) El promedio de todos los números positivos ingresados. 
+      *  e) La suma de todos los números negativos. 
+      *  f) El número y la letra del máximo y el mínimo.
+     **/
+
+    if (ingresoPositivo){
+        promedioPositivos = sumaPositivos/contadorPositivos;
+    }
+    else{
+        promedioPositivos="N/A";
+    }
+
+    document.write("a) Cantidad de números pares: "+contadorPares+"<br>");
+    document.write("b) Cantidad de números impares: "+contadorImpares+"<br>");
+    document.write("c) Cantidad de ceros: "+contadorCeros+"<br>");
+    document.write("d) Promedio de todos los números positivos ingresados: "+promedioPositivos+"<br>");
+    document.write("e) Suma de todos los números negativos: "+sumaNegativos+"<br>");
+    document.write("f1) Número y la letra del máximo: "+numeroMaximo+letraMaximo+"<br>");
+    document.write("f2) Número y la letra del mínimo: "+numeroMinimo+letraMinimo+"<br>");
 }
